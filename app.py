d15381351fa600515dedc5d7480477b4f0b376ab
Naomi Nishimura <nishimura.naomi.qq8@p.jp.nssol.nipponsteel.com>
2024-05-28 16:19:26 +0900

    5th commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -10,4 +11,4 @@ def receive_json():
     return jsonify(data), 200
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(debug=True,port=os.getenv("PORT"))
