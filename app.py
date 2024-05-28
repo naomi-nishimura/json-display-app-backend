a3c4807832a137f4bee5e8480af88da9f861d991
Naomi Nishimura <nishimura.naomi.qq8@p.jp.nssol.nipponsteel.com>
2024-05-28 15:49:43 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/json', methods=['POST'])
+def receive_json():
+    data = request.json
+    return jsonify(data), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
