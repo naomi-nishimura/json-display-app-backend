ecba91b9512f6e3bf1ef39db132e2dcf7f9fe87a
Naomi Nishimura <nishimura.naomi.qq8@p.jp.nssol.nipponsteel.com>
2024-05-28 17:41:16 +0900

    ホームページ作成

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
+import json
+from datetime import datetime
+
+now = datetime.now()
+whole_data = []
 
 app = Flask(__name__)
 CORS(app)
@@ -8,7 +13,15 @@ CORS(app)
 @app.route('/json', methods=['POST'])
 def receive_json():
     data = request.json
+    whole_data.append({"createdOn":str(now),"data":data})
     return jsonify(data), 200
+
+@app.route('/')
+def index():
+    html_string = ""
+    for jsonObj in whole_data:
+        html_string += f"<p>{jsonObj}</p>"
+    return f'<html><h1>テスト結果</h1>{html_string}</html>'
 
 if __name__ == '__main__':
     app.run(debug=True)
